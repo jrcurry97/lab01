3735033b34a4402fd04a4e00943a10265dab041b
jrcurry97 <jrcurry97@gwu.edu>
2016-01-15 22:23:26 +0000

    Homework 1

--- hello.c
+++ hello.c
@@ -7,5 +7,20 @@
 int main(int argc, char *argv[])
 {
 	printf("Hello world!\n");
+	
+	int x,y,z;
+	x=10;
+	y=20;
+	z=x+y;
+	printf("x is %d, y is %d, z is %d\n",x,y,z);
+	
+	int h,w,d,v ; /*h=height, w=width, d=depth, v=volume*/
+	
+	h=11;
+	w=7;
+	d=14;
+	v=h*w*d;
+	printf("Volume of the box is %d\n",v);
+	
 	return 0;
 }
